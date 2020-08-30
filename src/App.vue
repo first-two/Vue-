e46c692669ba6b4919a92696b8f04b72b08ddfa1
haoxiaoliang <1013621266@qq.com>
2020-08-30 15:57:54 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- 顶部导航栏 -->
+    <mt-header fixed title="mint-ui"></mt-header>
+    <!-- 路由切换区 -->
+    <router-view/>
+    <!-- 底部tab -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+
+  </div>
+</template>
+
+<style lang="less">
+  #app{padding-top: 40px;}
+</style>
